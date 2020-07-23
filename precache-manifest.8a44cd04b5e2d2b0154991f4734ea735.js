def1a503432cc4c97ccf6c11bf489851a06f4b2f
Ikenna_1246 <pacificheights2008@hotmail.com>
2020-07-23 15:24:16 +0100

    Updates

---
+++ precache-manifest.8a44cd04b5e2d2b0154991f4734ea735.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c9a67d50431d5bb9a601f0699c5f96d",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "e9ac2b29f93cf28c4bad",
+    "url": "/contactmanager/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "0ade2eed431287d04ba3",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "e9ac2b29f93cf28c4bad",
+    "url": "/contactmanager/static/js/2.0e8a9964.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager/static/js/2.0e8a9964.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ade2eed431287d04ba3",
+    "url": "/contactmanager/static/js/main.98170165.chunk.js"
+  },
+  {
+    "revision": "78370bb576dc7a6cd44e",
+    "url": "/contactmanager/static/js/runtime-main.cdf6a9fb.js"
+  }
+]);
